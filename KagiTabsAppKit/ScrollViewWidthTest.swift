298e3eb01cdb76770cffb5304bbf270e9eb82c0a
Andy Park <andy@bigbearlabs.com>
2023-11-24 17:42:12 +0900

    checkpoint layout constraints that will play nicely when presented in toolbar (unfinished)

---
+++ KagiTabsAppKit/ScrollViewWidthTest.swift
@@ -0,0 +1,8 @@
+//
+//  ScrollViewWidthTest.swift
+//  KagiTabsAppKit
+//
+//  Created by ilo on 24/11/2023.
+//
+
+import Foundation
---
+++ KagiTabsAppKit/TestScrollViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TestScrollViewController.swift
+//  KagiTabsAppKit
+//
+//  Created by ilo on 24/11/2023.
+//
+
+import Cocoa
+
+class TestScrollViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
