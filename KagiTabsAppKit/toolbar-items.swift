a968b1067709a7e16d1b8fe0b294337af9afb9c0
Andy Park <andy@bigbearlabs.com>
2023-11-26 08:45:15 +0900

    refine browser toolbar item configuration

--- KagiTabsAppKit/toolbar-items.swift
+++ KagiTabsAppKit/toolbar-items.swift
@@ -5,15 +5,19 @@ class BrowserToolbarItem: NSToolbarItem {
   var browserToolbarViewController: BrowserToolbarViewController
   
   init(itemIdentifier: NSToolbarItem.Identifier, viewModel: ToolbarViewModel) {
-    guard let browserToolbarViewController = NSStoryboard(name: .init("BrowserToolbar"), bundle: nil).instantiateInitialController() as? BrowserToolbarViewController
+    guard let browserToolbarViewController = NSStoryboard(name: .init("BrowserToolbar"), bundle: nil)
+      .instantiateInitialController() as? BrowserToolbarViewController
     else { fatalError() }
-    self.browserToolbarViewController = browserToolbarViewController
-
-    super.init(itemIdentifier: itemIdentifier)
     
     browserToolbarViewController.viewModel = viewModel
 
+    self.browserToolbarViewController = browserToolbarViewController
+    super.init(itemIdentifier: itemIdentifier)
+
     self.view = browserToolbarViewController.view
+    
+    self.label = "Address and Tabs"
+    self.paletteLabel = "Address and Tabs"
   }
 }
 
