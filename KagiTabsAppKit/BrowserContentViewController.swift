cfd74f26e9a49bd11b987991546b8ab97eb65d53
Andy Park <andy@bigbearlabs.com>
2023-11-29 17:51:41 +0900

    subscribe to webview title changes and update tab.label

--- KagiTabsAppKit/BrowserContentViewController.swift
+++ KagiTabsAppKit/BrowserContentViewController.swift
@@ -25,16 +25,28 @@ class BrowserContentViewController: NSViewController {
     super.viewDidLoad()
     
     subscriptions = [
+      // view model observations
       self.publisher(for: \.tab?.url)
         .sink { [unowned self] url in
-        if webView.url != url,
-          let url = url {
-          let request = URLRequest(url: url)
-          webView.load(request)
-        }
-      }
+          if webView.url != url,
+            let url = url {
+            let request = URLRequest(url: url)
+            webView.load(request)
+          }
+        },
+      
+      // view observations
+      self.publisher(for: \.webView?.title)
+        .combineLatest(
+          self.publisher(for: \.webView?.url).prepend(nil)
+        )
+        .sink { [unowned self] title, url in
+          if url != nil,
+             let title = title {
+            self.tab?.label = title
+          }
+        },
     ]
   }
   
 }
-
