4b2bc554ff6bc716abfdebbc78284ed1730a72a3
Andy Park <andy@bigbearlabs.com>
2023-11-30 17:12:11 +0900

    tidy up toolbar vc for legibility

--- KagiTabsAppKit/BrowserToolbarViewController.swift
+++ KagiTabsAppKit/BrowserToolbarViewController.swift
@@ -2,23 +2,53 @@ import AppKit
 
 
 
-// NOTE: in xib, we have a very tall fixed height, to ease testing inside the toolbar,
-// pending optimisation.
 class BrowserToolbarViewController: NSViewController {
   
+  /// the toolbar data to render.
+  @objc dynamic
+  var viewModel: ToolbarViewModel? {
+    didSet {
+      tabCollectionViewController?.viewModel = viewModel
+    }
+  }
+
+  /// manages all tab behaviour.
   var tabCollectionViewController: TabCollectionViewController? {
     didSet {
       tabCollectionViewController?.viewModel = viewModel
     }
   }
   
-  var viewModel: ToolbarViewModel? {
+  /// constraint defined in xib that sizes the address field appropriately based on tabs visibility.
+  @IBOutlet var addressFieldWidthWhenTabsShown: NSLayoutConstraint?
+
+
+  @objc dynamic
+  var isTabsVisible: Bool = false {
     didSet {
-      tabCollectionViewController?.viewModel = viewModel
+      NSAnimationContext.runAnimationGroup { context in
+        self.addressFieldWidthWhenTabsShown?.isActive = isTabsVisible
+        tabsContainerView?.animator().isHidden = !isTabsVisible
+        
+        self.view.layoutSubtreeIfNeeded()
+      }
     }
   }
-  
 
+  
+  var subscriptions: Any?
+  
+  
+  // MARK: view lifecycle
+  
+  override func viewDidAppear() {
+    super.viewWillAppear()
+    
+    self.subscriptions = viewModelSubscriptions
+    self.isTabsVisible = viewModel?.tabs.count ?? 0 > 1
+  }
+  
+  
   // MARK: storyboard lifecycle
   
   override func prepare(for segue: NSStoryboardSegue, sender: Any?) {
@@ -30,8 +60,29 @@ class BrowserToolbarViewController: NSViewController {
 //    self.view.setDebugBorder(.red)  // DEBUG
   }
 
+  
+  // MARK: misc
+  
   // doesn't work because not in responder chain. :|
+  // defined in window controller for now, until a better toolbar delegate is defined and wired up.
   @IBAction func addressFieldSubmitted(_ sender: NSTextField) {
 
+  }
+  
+  var viewModelSubscriptions: Any {
+    [
+      self.publisher(for: \.viewModel?.tabs, options: [.initial, .new])
+        .map { tabs in
+          let shouldShowTabs = (tabs?.count ?? 0) > 1
+          return shouldShowTabs
+        }
+        .assign(to: \.isTabsVisible, on: self)
+    ]
+  }
+  
+
+  // temp-outlet
+  var tabsContainerView: NSView? {
+    self.tabCollectionViewController?.view.superview
   }
 }
